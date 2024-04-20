178b32d9cd13743a285f73729b2360a18d8c502f
Oadeshina55 <123444511+Oadeshina55@users.noreply.github.com>
2024-04-20 20:16:22 +0000

    navbar

---
+++ joule-jesters/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import './Navbar.css'
+
+
+function Navbar() {
+
+    return(
+        <div>
+            <nav className="dark-nav">
+                <img />
+                <ul>
+                    <li>Home</li>
+                    <li>Our Services</li>
+                    <li>About Us</li>
+                    <button className="btn">Contact Us</button>
+                </ul>
+            </nav>
+        </div>
+    )
+}
